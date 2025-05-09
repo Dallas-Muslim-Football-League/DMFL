7a4b38a199b7c8a79f88fb806aac3dbf85e74af1
pfellows10014@gmail.com <patrick10014@icloud.com>
2025-05-08 20:24:18 -0500

    site update 3.0

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   backend_url: 'https://backend.d-mfl.com/api',
   apiUri: 'https://d-mfl.com',
-  assetBasePath: 'DMFL/assets',
+  assetBasePath: 'assets',
 };
